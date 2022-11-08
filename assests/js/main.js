89c3796fe2516eafb7148120dc346d3dcfc9f870
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-08 16:14:04 +0100

    Creazione delle mail random attarverso ciclo for

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,9 +1,25 @@
+
+// Attraverso l'apposita API di Boolean 
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        
+        randomEmail:[]
       }
+    },
+    mounted(){
+        for (let i = 1; i <= 10; i++) {
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(response =>{
+             console.log(response.data.response);
+             this.randomEmail.push(response.data.response);
+            })
+       }
     }
   }).mount('#app')
