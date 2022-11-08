632c8d663ce6d9043361fd101dfe7456a3d151f5
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-08 17:00:36 +0100

    end Vue Email List

--- assests/js/main.js
+++ assests/js/main.js
@@ -10,15 +10,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        randomEmail:[]
+        randomEmail:[],
       }
     },
     mounted(){
         for (let i = 1; i <= 10; i++) {
             axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
             .then(response =>{
-             console.log(response.data.response);
-             this.randomEmail.push(response.data.response);
+                // console.log(response.data.response);
+                this.randomEmail.push(response.data.response);
             })
        }
     }
